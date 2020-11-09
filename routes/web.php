a1d801b226c58c8826081708a3d2e424833e8697
krs91tk <krs91tk@gmail.com>
2020-11-09 22:29:02 +0900

    add calender

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
@@ -32,4 +29,5 @@ Route::group(['prefix' => 'admin'], function() {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/', 'NewsController@index');
+//Route::get('/', 'NewsController@index');
+Route::get('/', 'CalendarController@show');
